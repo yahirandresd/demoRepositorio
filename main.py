943adfe860594fb70d4a1a2255d84b009208b176
Yahir Rangel <yahirandres.rangel@gmail.com>
2024-09-11 08:49:20 -0500

    4987854

--- main.py
+++ main.py
@@ -11,10 +11,6 @@ def division(a, b):
     return a/b
 
 def multiplicacion(a,b):
-<<<<<<< HEAD
     return a-b
-=======
-    return a+b
->>>>>>> 94b595a8328f5b66f1e60f7a32ceff8b7f4bc8c7
 
 resultado=suma(7,5)
